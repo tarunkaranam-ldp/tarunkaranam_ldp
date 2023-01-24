8cb008fb41dd7234158f6cbf1066c7e6efe51476
tarunkaranam-ldp <118796562+tarunkaranam-ldp@users.noreply.github.com>
2023-01-24 21:03:39 +0530

    updated java

--- src/App.java
+++ src/App.java
@@ -3,9 +3,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         String msg = "Ab36Uz@9";
         encoder en = new encoder();
-        String s = en.encode(msg);
-        String o = en.decode(s);
-        System.out.println(s);
-        System.out.println(o);
+        String EncodingMessage = en.encode(msg);
+        String DecodingMessage = en.decode(EncodingMessage);
+        System.out.println(EncodingMessage);
+        System.out.println(DecodingMessage);
     }
 }
